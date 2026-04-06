5a49582b4c20d1a10fd46757ce74148b58cfd1c2
juliawheatman <julia.wheatman@gmail.com>
2026-04-06 21:31:13 +0300

    add try-catch

--- pshenitsyna.julia/task2/main.js
+++ pshenitsyna.julia/task2/main.js
@@ -1,7 +1,8 @@
 "use strict"
 
 function maxSize(...arrays){
-    let nArrays = arrays[0].length;
+    try {
+        let nArrays = arrays[0].length;
     let size = 0;
     for (let i = 0; i < nArrays; ++i){
         if (arrays[0][i].length>size){
@@ -9,20 +10,29 @@ function maxSize(...arrays){
         }
     }
     return size;
+    } catch (error) {
+        console.log(error.message);
+    }
+    
 }
 
 function zipArrays(...arrays){
-    let arr_res = []; 
+    try {
+        let arr_res = []; 
     let size = maxSize(arrays);
     for (let i = 0; i < size; ++i){
-        let arr_temp = []
+        let arr_temp = [];
         for (let arr of arrays){
             if (arr[i] == undefined) continue;
             arr_temp.push(arr[i]);
         }
-        arr_res.push(arr_temp)
+        arr_res.push(arr_temp);
     }
     return arr_res;
+    } catch (error) {
+        console.log(error.message);
+    }
+    
 }
 
 // проверка
@@ -31,3 +41,4 @@ let arr2 = ['a', 'b', 'c', 'd', 'e'];
 let arr3 = [1, , 4, 7, 8, 9, 0]
 // ожидаемый результат: [[1, 'a', 1], [2, 'b'], [3, 'c', 4], [4, 'd', 7], [5, 'e', 8], [9], [0]]
 console.log(zipArrays(arr1, arr2, arr3))
+console.log(zipArrays(arr1, arr2, undefined))
