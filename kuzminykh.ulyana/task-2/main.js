c20de2933915585a9b9109be755f47187b7c48b2
Ulyashkass <hukumka1978@gmail.com>
2026-04-06 17:10:24 +0300

    [TASK-2]

---
+++ kuzminykh.ulyana/task-2/main.js
@@ -0,0 +1,30 @@
+let map = {
+    '01':{signs:['козерог','водолей'], num: 20},
+    '02':{signs:['водолей','рыбы'], num: 20},
+    '03':{signs:['рыбы','овен'], num: 20},
+    '04':{signs:['овен','телец'], num: 20},
+    '05':{signs:['телец','близнецы'], num: 20},
+    '06':{signs:['близнецы','рак'], num: 21},
+    '07':{signs:['рак','лев'], num: 22},
+    '08':{signs:['лев','дева'], num: 23},
+    '09':{signs:['дева','весы'], num: 23},
+    '10':{signs:['весы','скорпион'], num: 23},
+    '11':{signs:['скорпион','стрелец'], num: 22},
+    '12':{signs:['стрелец','козерог'], num: 21},
+};
+
+function zodiacsign(data) {
+    let num = data.substr(0,2);
+    let mon = data.substr(3,2);
+    let number = +num;
+    let month = map[mon];
+    if (number <= month.num){
+        console.log(month.signs[0])
+    } else {
+        console.log(month.signs[1])
+    }
+}
+
+zodiacsign('06.01.2007');
+zodiacsign('20.02.1997');
+zodiacsign('03.08.2012');
