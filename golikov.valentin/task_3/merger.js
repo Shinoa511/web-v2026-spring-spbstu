8429624ebb12a5f83b7910f696ce760267e9919d
ValentinGolikov <triderm300@gmail.com>
2026-04-09 16:07:18 +0300

    upd

--- golikov.valentin/task_3/merger.js
+++ golikov.valentin/task_3/merger.js
@@ -1,22 +1,56 @@
 function mergeArrays(arr1, arr2) {
-    let result = [];
+
+    if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+        throw new TypeError("Оба аргумента должны быть массивами");
+    }
+
+    // let result = [];
     
-    // Копируем arr1 в result
+    // // Копируем arr1 в result
+    // for (let i = 0; i < arr1.length; i++) {
+    //     result.push(arr1[i]);
+    // }
+    
+    // // Добавляем в result те элементы из массива arr2, которых еще нет
+    // for (let i = 0; i < arr2.length; i++) {
+    //     if (!result.includes(arr2[i])) {
+    //         result.push(arr2[i]);
+    //     }
+    // }
+
+    let setter = new Set();
+
+    //копируем элементы из первого массива в Set
     for (let i = 0; i < arr1.length; i++) {
-        result.push(arr1[i]);
+        setter.add(arr1[i]);
     }
     
     // Добавляем в result те элементы из массива arr2, которых еще нет
     for (let i = 0; i < arr2.length; i++) {
-        if (!result.includes(arr2[i])) {
-            result.push(arr2[i]);
-        }
+        setter.add(arr2[i]);
     }
-    
-    return result;
+
+    // Превращаем Set в массив с помощью spread оператора
+    return [...setter];
+
+    //spread оператор - ...
+}
+try {
+    console.log(mergeArrays([1, 2, 3], [2, 3, 4]));
+    console.log(mergeArrays(['a', 'b', 'c', 'd'], ['d', 'e', 'f']));
+    console.log(mergeArrays([1, 2, 3], [4, 5, 6]));
+    console.log(mergeArrays([], []));
+    console.log(mergeArrays([1, 2], 5));
+}
+catch (error) {
+    console.log("Ошибка:", error.message);
 }
 
-console.log(mergeArrays([1, 2, 3], [2, 3, 4]));
-console.log(mergeArrays(['a', 'b', 'c', 'd'], ['d', 'e', 'f']));
-console.log(mergeArrays([1, 2, 3], [4, 5, 6]));
-console.log(mergeArrays([], []));
+
+// Несколько примеров работы spread оператора
+console.log("\nПример работы spread-оператора");
+const str = "Hello";
+console.log([...str]); // ['H', 'e', 'l', 'l', 'o']
+
+const numbers = [10, 20, 30, 40, 50];
+console.log(Math.max(...numbers)); // 50
